09fc0cfa3a5103e3455d1d0f17ac258dbbf47c20
Derek Stephen McLean <delboy1978uk@gmail.com>
2019-03-23 00:25:00 +0100

    add project 4

---
+++ project4/Project 4/Project 4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 4
+//
+//  Created by Derek Stephen McLean on 23/03/2019.
+//  Copyright © 2019 Derek Stephen McLean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ project4/README.md
@@ -0,0 +1,5 @@
+# project 4
+- create new project, single page iOS app
+- goto interface builder
+- select view controlle
+- Editor > Embed In > NavigationController
