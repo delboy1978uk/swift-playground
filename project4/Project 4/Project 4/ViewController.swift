b9d40bf4d780a7844d53bc6cb727904195a280a0
Derek Stephen McLean <delboy1978uk@gmail.com>
2019-03-23 13:44:36 +0100

    web page app project 4

--- project4/Project 4/Project 4/ViewController.swift
+++ project4/Project 4/Project 4/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let url = URL(string: "https://wingsoverscotland.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
 
-
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "independencelive.net", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "indylive.radio", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "randompublicjournal.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "thoughtcontrolscotland.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "wingsoverscotland.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        guard let actionTitle = action.title else {return}
+        guard let url = URL(string: "https://" + actionTitle) else {return}
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
 }
 
--- project4/README.md
+++ project4/README.md
@@ -3,3 +3,44 @@
 - goto interface builder
 - select view controlle
 - Editor > Embed In > NavigationController
+- in View controller
+```swift
+import WebKit
+```
+- add a var and method
+```swift
+var webView: WKWebView!
+    
+override func loadView() {
+    webView = WKWebView()
+    webView.navigationDelegate = self
+    view = webView
+}
+```
+- implement the delgate interface
+```swift
+class ViewController: UIViewController, WKNavigationDelegate {
+```
+- and add to view did load
+```swift
+let url = URL(string: "https://coinmarketcap.com")!
+webView.load(URLRequest(url: url))
+webView.allowsBackForwardNavigationGestures = true
+```
+## add navigation
+- under super.viewDidLoad()
+```swift
+navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+```
+- create openTapped() function
+```swift
+@objc func openTapped() {
+let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .actionSheet)
+ac.addAction(UIAlertAction(title: "indylive.radio", style: .default, handler: openPage))
+ac.addAction(UIAlertAction(title: "delboy1978uk.wordpress.com", style: .default, handler: openPage))
+ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+present(ac, animated: true)
+}
+```
+
