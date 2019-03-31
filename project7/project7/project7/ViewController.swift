fec7e7b6bb61d998d0f75c5ad3f94643c7815762
Derek Stephen McLean <delboy1978uk@gmail.com>
2019-03-31 19:08:28 +0200

    load json data, wire in detailviewvcontroller

--- project7/project7/project7/ViewController.swift
+++ project7/project7/project7/ViewController.swift
@@ -46,5 +46,11 @@ class ViewController: UITableViewController {
         }
     }
 
+    // hook up detailviewcontroller
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
 
