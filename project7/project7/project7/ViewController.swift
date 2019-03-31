d3b8080d16a453b2a3ebe1e66e41e12f91137a23
Derek Stephen McLean <delboy1978uk@gmail.com>
2019-03-31 04:28:10 +0200

    project 7 wip

---
+++ project7/project7/project7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project7
+//
+//  Created by Derek Stephen McLean on 30/03/2019.
+//  Copyright © 2019 Derek Stephen McLean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
