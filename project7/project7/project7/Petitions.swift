13d77820314757aca54df3190d34c41959bd7d91
Derek Stephen McLean <delboy1978uk@gmail.com>
2019-03-31 18:56:04 +0200

    fetch json

---
+++ project7/project7/project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  project7
+//
+//  Created by Derek Stephen McLean on 31/03/2019.
+//  Copyright © 2019 Derek Stephen McLean. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
