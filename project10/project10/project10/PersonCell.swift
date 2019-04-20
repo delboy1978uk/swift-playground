d98b09b8abc5775f3db78f11cdff54d0cf117047
Derek Stephen McLean <delboy1978uk@gmail.com>
2019-04-20 23:04:12 +0200

    setting up interface for name to face app

---
+++ project10/project10/project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  project10
+//
+//  Created by Derek Stephen McLean on 20/04/2019.
+//  Copyright © 2019 Derek Stephen McLean. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
---
+++ project10/project10/project10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project10
+//
+//  Created by Derek Stephen McLean on 20/04/2019.
+//  Copyright © 2019 Derek Stephen McLean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
