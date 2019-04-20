e89a2b82c776348480b96ac7d3140355e0bdea26
Derek Stephen McLean <delboy1978uk@gmail.com>
2019-04-20 22:03:25 +0200

    project 9 finished

--- project9/README.md
+++ project9/README.md
@@ -46,3 +46,60 @@ func showError() {
 }
 ```
 ## performSelector(inBackground:)
+- use `performSelector(inBackground:)` and `performSelector(onMainThread:)` to refactor
+```swift
+override func viewDidLoad() {
+    super.viewDidLoad()
+
+    performSelector(inBackground: #selector(fetchJSON), with: nil)
+}
+
+@objc func fetchJSON() {
+    let urlString: String
+
+    if navigationController?.tabBarItem.tag == 0 {
+        urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+    } else {
+        urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+    }
+
+    if let url = URL(string: urlString) {
+        if let data = try? Data(contentsOf: url) {
+            parse(json: data)
+            return
+        }
+    }
+
+    performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+}
+
+func parse(json: Data) {
+    let decoder = JSONDecoder()
+
+    if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+        petitions = jsonPetitions.results
+        tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
+    }
+}
+
+@objc func showError() {
+    let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+    ac.addAction(UIAlertAction(title: "OK", style: .default))
+    present(ac, animated: true)
+}
+```
+- and add an else to the parse() mnethod
+```swift
+if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+    petitions = jsonPetitions.results
+    tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
+} else {
+    performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+}
+```
+## challenges
+@todo
+- Modify project 1 so that loading the list of NSSL images from our bundle happens in the background. Make sure you call reloadData() on the table view once loading has finished!
+- Modify project 8 so that loading and parsing a level takes place in the background. Once you’re done, make sure you update the UI on the main thread!
+- Modify project 9 so that your filtering code takes place in the background. This filtering code was added in one of the challenges for project 9, so hopefully you didn’t skip it!
+
--- project9/project9/project7/ViewController.swift
+++ project9/project9/project7/ViewController.swift
@@ -15,26 +15,45 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        performSelector(inBackground: #selector(fetchJSON), with: nil)
+    }
+    
+    @objc func fetchJSON() {
         let urlString: String
         
         if navigationController?.tabBarItem.tag == 0 {
-            // urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
             urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
         } else {
-            // urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
             urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
         }
         
-        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
-            if let url = URL(string: urlString) {
-                if let data = try? Data(contentsOf: url) {
-                    self?.parse(json: data)
-                    return
-                }
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+                return
             }
-            self?.showError()
+        }
+        
+        performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
+        } else {
+            performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
         }
     }
+    
+    @objc func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return petitions.count
@@ -46,32 +65,6 @@ class ViewController: UITableViewController {
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
-    }
-    
-    func parse(json: Data) {
-        let decoder = JSONDecoder()
-        
-        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
-            petitions = jsonPetitions.results
-            DispatchQueue.main.async { [weak self] in
-                self?.tableView.reloadData()
-            }
-        }
-    }
-
-    // hook up detailviewcontroller
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let vc = DetailViewController()
-        vc.detailItem = petitions[indexPath.row]
-        navigationController?.pushViewController(vc, animated: true)
-    }
-    
-    func showError() {
-        DispatchQueue.main.async { [weak self] in
-            let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default))
-            self?.present(ac, animated: true)
-        }
     }
     
     
